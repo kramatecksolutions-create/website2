14da7ee5e591171fc0e239660d03161e1544f74f
kramatecksolutions-create <kramatecksolutions@gmail.com>
2026-09-04 23:18:24 -0400

    Add files via upload

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,4 @@
+
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+export default function NotFound(){return <section className="notfound"><div><span>404</span><h1>Page not found.</h1><NavLink className="btn primary" to="/">Back Home</NavLink></div></section>}
