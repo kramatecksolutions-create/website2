b0accfbb2b1cccf04a9f2e670bfdef066a904d28
kramatecksolutions-create <kramatecksolutions@gmail.com>
2026-09-04 21:27:14 -0400

    Delete src directory

--- src/pages/NotFound.jsx
+++
@@ -1,4 +0,0 @@
-
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-export default function NotFound(){return <section className="page-hero"><div className="container"><div className="eyebrow">404</div><h1>Page not found.</h1><NavLink className="btn primary" to="/">Back Home</NavLink></div></section>}
